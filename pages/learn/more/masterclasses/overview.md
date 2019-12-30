50127baf2464d75a661dd94ae6916b517246d6a3
Gareth Davies <hello@garethtdavies.com>
2019-12-30 13:23:44 -0800

    WIP: docs: Embedding masterclasses into documentation
    Change-type: minor
    Signed-off-by: Gareth Davies gareth@balena.io

---
+++ pages/learn/more/masterclasses/overview.md
@@ -0,0 +1,17 @@
+---
+title: Overview
+---
+
+# {{ $names.company.upper }} Masterclasses
+
+Masterclasses are a series of self-contained, in-depth exercises designed to take you from a {{ $names.company.lower }} beginner to power user. Each masterclass focuses on providing in-depth knowledge on a specific part of the {{ $names.company.lower }} system.
+
+Feedback on the masterclasses are welcome and should be given by creating an issue in the respective masterclass repository.
+
+Currently, the following masterclasses are available:
+
+* [{{ $names.cli }} masterclass](/learn/more/masterclasses/cli-masterclass/) - a guide to getting started with the {{ $names.cli }}.
+* [{{ $names.cli }} advanced masterclass](/learn/more/masterclasses/advanced-cli-masterclass/) - a guide to the advanced features available in {{ $names.cli }}.
+* [{{ $names.os.lower }} masterclass](/learn/more/masterclasses/host-os-masterclass/) - an introduction to {{ $names.os.lower }}.
+* [Services masterclass](/learn/more/masterclasses/services-masterclass/) - a guide to developing and deploying services on a {{ $names.company.lower }} device.
+* [{{ $names.company.lower }} fleet management masterclass](/learn/more/masterclasses/fleet-management-masterclass/) - a guide to managing an application fleet with {{ $names.company.lower }}.
--- tools/fetch-external.sh
+++ tools/fetch-external.sh
@@ -2,28 +2,57 @@
 # This script pulls in external documentation that should be edited in the corresponding upstream repo
 
 # get latest CLI docs
-cd pages/reference/ && { curl -O -L https://github.com/balena-io/balena-cli/raw/master/doc/cli.markdown ; mv cli.markdown cli.md ; cd -; }
+cd pages/reference/ && {
+  curl -O -L https://github.com/balena-io/balena-cli/raw/master/doc/cli.markdown
+  mv cli.markdown cli.md
+  cd -
+}
 
 # get latest node SDK docs
 cd pages/reference/sdk/ && {
-  curl -O -L https://github.com/balena-io/balena-sdk/raw/master/DOCUMENTATION.md;
-  echo "# Balena Node.js SDK" > node-sdk.md
-  tail -n +2 DOCUMENTATION.md >> node-sdk.md;
-  cd -;
+  curl -O -L https://github.com/balena-io/balena-sdk/raw/master/DOCUMENTATION.md
+  echo "# Balena Node.js SDK" >node-sdk.md
+  tail -n +2 DOCUMENTATION.md >>node-sdk.md
+  cd -
 }
 
 # get latest python SDK docs
 cd pages/reference/sdk/ && {
-  curl -O -L https://github.com/balena-io/balena-sdk-python/raw/master/DOCUMENTATION.md;
-  mv DOCUMENTATION.md python-sdk.md;
-  cd -;
+  curl -O -L https://github.com/balena-io/balena-sdk-python/raw/master/DOCUMENTATION.md
+  mv DOCUMENTATION.md python-sdk.md
+  cd -
 }
 
 # get latest supervisor API docs
-cd pages/reference/supervisor/ && { curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/API.md; mv API.md supervisor-api.md ; cd -; }
+cd pages/reference/supervisor/ && {
+  curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/API.md
+  mv API.md supervisor-api.md
+  cd -
+}
 
 # get latest diagnostics docs
-cd pages/reference/ && { curl -O -L https://github.com/balena-io/device-diagnostics/raw/master/diagnostics.md; cd -; }
+cd pages/reference/ && {
+  curl -O -L https://github.com/balena-io/device-diagnostics/raw/master/diagnostics.md
+  cd -
+}
 
 # get latest supervisor update-lock docs
-cd pages/learn/deploy/release-strategy/ && { curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/update-locking.md; cd -; }
+cd pages/learn/deploy/release-strategy/ && {
+  curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/update-locking.md
+  cd -
+}
+
+# Masterclasses
+cd pages/learn/more/masterclasses/ && {
+  curl -O -L https://raw.githubusercontent.com/balena-io-projects/balena-cli-masterclass/master/README.md
+  mv README.md cli-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io-projects/balena-cli-advanced-masterclass/master/README.md
+  mv README.md advanced-cli-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io-projects/balenaos-masterclass/master/README.md
+  mv README.md host-os-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io-projects/services-masterclass/master/README.md
+  mv README.md services-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io-projects/balena-fleet-management-masterclass/master/README.md
+  mv README.md fleet-management-masterclass.md
+  cd -
+}
